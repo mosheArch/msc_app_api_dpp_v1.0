82c591cec1fc80afc0ac2c23137929a51ac2ec06
Alvaro M Diaz Diaz <alvaro.moises.diaz.diaz@gmail.com>
2022-02-27 22:25:20 -0600

    apis para resultados del cuestionario

--- edimburgo/views.py
+++ edimburgo/views.py
@@ -7,39 +7,6 @@ from rest_framework.views import APIView
 from core.models.model_edimburgo import CuestionarioEdimburgo, resultdoEdimburgo
 from edimburgo import serializers
 
-
-# class EdimburgoViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
-#     authentication_classes = (TokenAuthentication,)
-#     permission_classes = (IsAuthenticated,)
-#     queryset = CuestionarioEdimburgo.objects.all()
-#     serializer_class = serializers.datos_cuestionarioEdimburgo_serializer
-#
-#     def get_queryset(self):
-#         return self.queryset.filter(user=self.request.user).order_by('user_id')
-#
-#
-# # class CrearCuestionarioViewSet(EdimburgoViewSet):
-# #     """Manage tags in the database"""
-# #     queryset = CuestionarioEdimburgo.objects.all()
-# #     serializer_class = serializers.datos_cuestionarioEdimburgo_serializer
-
-# class DatosListsView_Edimburgo(APIView, mixins.ListModelMixin, mixins.CreateModelMixin):
-#     authentication_classes = (TokenAuthentication,)
-#     permission_classes = (IsAuthenticated,)
-#
-#     def get(self, request):
-#         datos = CuestionarioEdimburgo.objects.all()
-#         serializer = serializers.datos_cuestionarioEdimburgo_serializer(datos, many=True)
-#         return Response(serializer.data)
-#
-#     def post(self, request):
-#         serializer = serializers.datos_cuestionarioEdimburgo_serializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class DatosListsView_Edimburgo(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
